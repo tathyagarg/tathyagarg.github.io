ba3c10a498015b03613fae2bd8afe76294889120
Tathya Garg <tathya.garg@gmail.com>
2024-03-19 02:23:58 +0530

    Removed useless stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import AboutMeSection from "../components/aboutme.js"
-import PortfolioSection from "../components/portfolio.js";
 import BlogSection from "../components/blog.js";
-import HobbiesSection from "../components/hobbies.js";
 
 const Home = () => {
     return (
         <div className="page">
             <AboutMeSection/>
             <BlogSection/>
-            <PortfolioSection/>
-            <HobbiesSection/>
         </div>
     )
 }
